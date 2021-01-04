d0c330f111ae9028ab84388421320804c595ef70
g.nazarov <79031971990@yandex.ru>
2021-01-04 17:11:08 +0300

    Опять что-то в тегах поменялось, поправил

--- swipe_script.js
+++ swipe_script.js
@@ -3,13 +3,14 @@ function sleep(ms) {
 }
 
 var enable_swiping = true;
-var swiping_speed = 1000;
+var swiping_speed = 300;
 var like_cnt = 0;
 var dis_cnt = 0;
-
-await sleep(swiping_speed);
+var no_discription_cnt = 0;
 
 while (enable_swiping == true) {
+
+await sleep(swiping_speed);
 
 /*
 Посчитать количество фотографий
@@ -33,12 +34,14 @@ close_info_button.click();
 
 //need to wait a second to load info
 await sleep(swiping_speed);
+
 let curr_prof_info = document.getElementsByClassName('P(16px) Ta(start) Us(t) C($c-secondary) BreakWord Whs(pl) Fz($ms)');
 //if function in "try" ends with error, there are no discripton on profile
 try {
     description_length = curr_prof_info[0].innerText.length;
 } catch (err) {
     description_length = 0;
+    no_discription_cnt++;
     console.log("Нет описания")
 }
 
@@ -51,7 +54,7 @@ await sleep(swiping_speed);
 let like_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a')[1];
 let dislike_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a')[0];
 
-if ( photo_quantity > 1 && description_length > 30) {
+if ( photo_quantity > 1 && description_length > 150) {
     console.log("Like");
     like_cnt++;
     like_button.click();
