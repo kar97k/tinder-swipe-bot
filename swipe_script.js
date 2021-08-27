b2ad93fb46f1a72f2e6246aa5408947febf79c9f
g.nazarov <79031971990@yandex.ru>
2021-08-27 19:34:47 +0300

    1)Fixed tag names 2)Output description in the log

--- swipe_script.js
+++ swipe_script.js
@@ -29,14 +29,14 @@ await sleep(swiping_speed);
 var description_length = 0;
 
 //click button "profile info"
-let close_info_button = document.getElementsByClassName('P(0) Trsdu($normal) Sq(28px) Bdrs(50%) Cur(p) Ta(c) Scale(1.2):h Mb(12px)--ml Mb(8px) focus-button-style')[1];
-close_info_button.click();
+let info_button = document.getElementsByClassName('P(0) Trsdu($normal) Sq(28px) Bdrs(50%) Cur(p) Ta(c) Scale(1.2):h Mb(12px)--ml Mb(8px) focus-button-style')[1];
+info_button.click();
 
 //need to wait a second to load info
 await sleep(swiping_speed);
 
-let curr_prof_info = document.getElementsByClassName('P(16px) Ta(start) Us(t) C($c-secondary) BreakWord Whs(pl) Fz($ms)');
-//if function in "try" ends with error, there are no discripton on profile
+let curr_prof_info = document.getElementsByClassName('P(16px) Us(t) C($c-secondary) BreakWord Whs(pl) Fz($ms)');
+//if function in "try" ends with error, there is no discripton on profile
 try {
     description_length = curr_prof_info[0].innerText.length;
 } catch (err) {
@@ -45,17 +45,16 @@ try {
     console.log("Нет описания")
 }
 
-//close "profile info"
-//let info_button = document.getElementsByClassName('End(12px) B(-20px) Pos(a) Z(2) CenterAlign Bdrs(50%) P(0) Scale(1.1):h Trsdu($normal) focus-button-style')[0];
-//info_button.click();
 await sleep(swiping_speed);
 
 //init like and dislike buttons
-let like_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a')[1];
-let dislike_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a')[0];
+let like_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgc(#fff)')[1];
+
+let dislike_button = document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgc(#fff)')[0];
 
 if ( photo_quantity > 1 && description_length > 100) {
     console.log("Like");
+    console.log(curr_prof_info[0].innerText);
     like_cnt++;
     like_button.click();
 } else {
